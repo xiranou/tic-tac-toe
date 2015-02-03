db155fc196f69bc78508c79dab8c4493bc25f7ba
xiranou <xiranou@gmail.com>
2015-02-03 13:54:05 -0500

    add test for mark method

--- tic_tac_toe_spec.rb
+++ tic_tac_toe_spec.rb
@@ -1,9 +1,21 @@
 require 'rspec'
 require_relative './tic_tac_toe.rb'
 
-describe "TicTacToeBoard" do
-  it "should initialize with a default 3x3 grid" do
-    board = TicTacToeBoard.new
-    expect(board.grid.size).to eq(9)
+describe TicTacToeBoard do
+
+  before do
+    @board = TicTacToeBoard.new
   end
+  it "should initialize with a default 3x3 grid" do
+    expect(@board.grid.size).to eq(9)
+  end
+
+  describe "mark" do
+    it "should mark the given spot with the given mark" do
+      move = {index: 0, mark: 'x'}
+      @board.mark(move)
+      expect(@board.grid[0]).to eq("x")
+    end
+  end
+
 end
