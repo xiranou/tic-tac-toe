a9718ba01006fdfe58095994afc3962aae036670
xiranou <xiranou@gmail.com>
2015-02-03 14:11:38 -0500

    add grid_size to intialize

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,14 +1,19 @@
 class TicTacToeBoard
 
-  attr_reader :grid, :size
+  attr_reader :grid, :size, :grid_size
 
   def initialize(size=3)
     @size = size
-    @grid = Array.new(size*size) { |spot| "_" }
+    @grid_size = size * size
+    @grid = Array.new(grid_size) { |spot| "_" }
   end
 
   def mark(move)
     grid[move[:index]] = move[:mark]
+  end
+
+  def horizontal_win?
+
   end
 
 end
