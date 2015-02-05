24a38a7c625e295ab15ca49e22f3fd1c9fe73db8
xiranou <xiranou@gmail.com>
2015-02-05 13:53:58 -0500

    add spec for at marking

--- ttt_ai_spec.rb
+++ ttt_ai_spec.rb
@@ -4,13 +4,25 @@ require './tic_tac_toe'
 
 describe "TttAi" do
   before do
-    board = TicTacToeBoard.new
-    @computer = TttAi.new({board: board, mark: "o"})
+    @board = TicTacToeBoard.new
+    @computer = TttAi.new({board: @board, mark: "o"})
   end
 
   describe "#free_spots" do
     it "should return array of indexes of free spots on board grid" do
       expect(@computer.free_spots).to match_array([*0..8])
+    end
+  end
+
+  describe "#mark_spot" do
+    before do
+      @computer.mark_spot
+    end
+    it "should mark to an empty spot" do
+      expect(@board.grid.include?("o")).to be true
+    end
+    it "should only mark once" do
+      expect(@board.grid.count("o")).to eq(1)
     end
   end
 end
