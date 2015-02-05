858a89fb70fa18c460bc69e72bac6cc89735853f
xiranou <xiranou@gmail.com>
2015-02-05 15:41:30 -0500

    add initial tests for game turns

--- game_spec.rb
+++ game_spec.rb
@@ -4,7 +4,7 @@ require './game_ai'
 require './game'
 
 describe "Game" do
-  before do
+  before :each do
     @game = Game.new
   end
 
@@ -17,6 +17,25 @@ describe "Game" do
     end
     it "should return the computer as 'o' mark" do
       expect(@game.computer.mark).to eq("o")
+    end
+  end
+
+  describe "turn" do
+    before do
+      player_move = {index:0, mark:"x"}
+      @game.game_turn(player_move)
+      @current_grid = @game.board.gird
+    end
+    context 'should alternate turns' do
+      it "should have one player mark" do
+        expect(@current_grid.find_all{|mark| mark == "x"}.count).to eq(1)
+      end
+      it "should have one computer mark" do
+        expect(@current_grid.find_all{|mark| mark == "o"}.count).to eq(1)
+      end
+    end
+    it "should keep track of turns" do
+      expect(@game.turn).to match_array(['x','o'])
     end
   end
 end
