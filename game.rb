268a4bc83c02e2626cd70e6208b559bb25cc21ad
xiranou <xiranou@gmail.com>
2015-02-06 16:22:21 -0500

    update initial game run

--- game.rb
+++ game.rb
@@ -3,38 +3,52 @@ require './game_ai'
 require './game_ui'
 
 class Game
-  attr_reader :board, :player, :computer, :marks, :turn
+  attr_accessor :board, :player, :computer
+  attr_reader :marks, :ai, :turn
 
   def initialize
     @board = TicTacToeBoard.new
     @marks = ["x","o"]
     @turn = []
+    @ai = GameAi.new(board)
   end
 
   def set_mark(choice)
-    @player = marks.delete(choice)
-    @computer = GameAi.new(board: board, mark: marks.last)
+    marks.each_with_index do |mark, index|
+      index == choice ? @player = mark : @computer = mark
+    end
   end
 
   def winner
     turn.last == player ? "player" : "computer"
   end
 
-  def game_turn(player_move)
-    turn << player << computer.mark
-    board.mark(player_move)
-    computer.mark_spot
+  def game_turn(current_turn)
+    if current_turn == player
+      move = {index: GameUi.player_move, mark: player}
+      board.mark(move)
+      turn << player
+    else
+      ai.mark_spot(computer)
+      turn << computer
+    end
   end
 
   def play
     player_choice = GameUi.choose_mark(marks)
     set_mark(player_choice)
+    GameUi.print_screen(board)
     until @board.finish?
-      GameUi.print_screen(board)
-      return GameUi.annouce_winner(winner) if board.game_win?
-      game_turn(GameUi.player_move(player))
+      marks.cycle do |current_turn|
+        game_turn(current_turn)
+        GameUi.print_screen(board)
+        if board.game_win?
+          GameUi.annouce_winner(winner)
+          play_again? ? play : return
+        end
+      end
     end
-    return "Draw!"
+    GameUi.annouce_winner("draw")
     play if play_again?
   end
 
