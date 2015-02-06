b9388d250fc5492e058eff0d406cfc33f99e3ad8
xiranou <xiranou@gmail.com>
2015-02-06 17:38:51 -0500

    update test for set_mark in Game

--- game_spec.rb
+++ game_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require './tic_tac_toe'
 require './game_ai'
+require './game_ui'
 require './game'
 
 describe "Game" do
@@ -8,39 +9,13 @@ describe "Game" do
   describe "should let player choose their marks" do
     before do
       @game = Game.new
-      @game.set_mark("x")
+      @game.set_mark(0)
     end
-    it "should return the player as 'x' mark" do
-     expect(@game.player).to eq("x")
+    it "should set player to 'x'" do
+      expect(@game.player).to eq('x')
     end
-    it "should return the computer as 'o' mark" do
-      expect(@game.computer.mark).to eq("o")
-    end
-  end
-
-  describe "turn" do
-    before do
-      @game = Game.new
-      @game.set_mark("x")
-      player_move = {index:0, mark:@game.player}
-      @game.game_turn(player_move)
-      @current_grid = @game.board.grid
-    end
-    context 'should alternate turns' do
-      it "should have one player mark" do
-        expect(@current_grid.find_all{|mark| mark == "x"}.count).to eq(1)
-      end
-      it "should have one computer mark" do
-        expect(@current_grid.find_all{|mark| mark == "o"}.count).to eq(1)
-      end
-    end
-    context "should keep track of turns" do
-      it "should have the right number of turns" do
-        expect(@game.turn.count).to eq(2)
-      end
-      it "should have the right order of turns" do
-        expect(@game.turn).to eq(["x", "o"])
-      end
+    it "should set computer to 'o'" do
+      expect(@game.computer).to eq('o')
     end
   end
 
