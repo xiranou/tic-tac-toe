dd9169bd783aa1be363a15fbddcb2ddabca70343
xiranou <xiranou@gmail.com>
2015-02-06 17:23:42 -0500

    refactor game play

--- game.rb
+++ game.rb
@@ -3,14 +3,13 @@ require './game_ai'
 require './game_ui'
 
 class Game
-  attr_accessor :board, :player, :computer
-  attr_reader :marks, :ai, :turn
+  attr_reader :board, :player, :computer, :ai, :marks, :turn
 
   def initialize
     @board = TicTacToeBoard.new
+    @ai = GameAi.new(board)
     @marks = ["x","o"]
     @turn = []
-    @ai = GameAi.new(board)
   end
 
   def set_mark(choice)
@@ -23,38 +22,58 @@ class Game
     turn.last == player ? "player" : "computer"
   end
 
+  def player_turn
+    move = {index: GameUi.player_move, mark: player}
+    board.mark(move)
+    turn << player
+  end
+
+  def ai_turn
+    ai.mark_spot(computer)
+    turn << computer
+  end
+
   def game_turn(current_turn)
-    if current_turn == player
-      move = {index: GameUi.player_move, mark: player}
-      board.mark(move)
-      turn << player
-    else
-      ai.mark_spot(computer)
-      turn << computer
+    current_turn == player ? player_turn : ai_turn
+  end
+
+  def player_choose_mark
+    player_choice = GameUi.choose_mark(marks)
+    set_mark(player_choice)
+  end
+
+  def game_round
+    marks.cycle do |current_turn|
+      game_turn(current_turn)
+      GameUi.print_screen(board)
+      return GameUi.annouce_winner(winner) if board.game_win?
     end
   end
 
   def play
-    player_choice = GameUi.choose_mark(marks)
-    set_mark(player_choice)
+    player_choose_mark
     GameUi.print_screen(board)
-    until @board.finish?
-      marks.cycle do |current_turn|
-        game_turn(current_turn)
-        GameUi.print_screen(board)
-        if board.game_win?
-          GameUi.annouce_winner(winner)
-          play_again? ? play : return
-        end
-      end
-    end
-    GameUi.annouce_winner("draw")
-    play if play_again?
+
+    game_round unless board.finish?
+
+    GameUi.annouce_winner("draw") unless board.game_win?
+
+    play_again? ? reset_game : GameUi.goodbye_prompt
+  end
+
+  def reset_game
+    self.board = TicTacToeBoard.new
+    self.ai = GameAi.new(board)
+    play
   end
 
   def play_again?
     GameUi.again_prompt == 'y'
   end
+
+  private
+
+  attr_writer :board, :ai
 end
 
 game = Game.new
