0d899482ed6d35027d61fda7ba65b427cd78576f
xiranou <xiranou@gmail.com>
2015-02-06 16:25:05 -0500

    add clear_screen to GameUi

--- game_ui.rb
+++ game_ui.rb
@@ -2,12 +2,19 @@ require './tic_tac_toe'
 module GameUi
   class << self
     def print_screen(board)
+      clear_screen
       grid, size = board.grid, board.size
       grid.each_slice(size) {|row| puts row.join(" | ")}
     end
+
+    def clear_screen
+      puts "\e[H\e[2J"
+    end
+
     def player_input
       gets.chomp.downcase
     end
+
     def choose_mark(marks)
       puts "Please choose your turn: #{marks.join(" / ")}"
       choice = player_input
@@ -18,14 +25,17 @@ module GameUi
         choose_mark(marks)
       end
     end
+
     def player_move
       move_index = player_input.to_i
     end
+
     def again_prompt(message='Play Again? (y/n)')
       puts message
       choice = player_input
       ['y','n'].include?(choice) ? choice : again_prompt("invalid choice!")
     end
+
     def annouce_winner(winner)
       puts "Draw Game!" if winner == 'draw'
       puts "#{winner.capitalize} is the winner!"
