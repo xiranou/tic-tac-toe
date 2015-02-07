4cc2f5876a1ce1b35eaafb9afedbfbc9eb055a6c
xiranou <xiranou@gmail.com>
2015-02-06 19:48:10 -0500

    move play.rb to root

--- game/play.rb
+++
@@ -1,6 +0,0 @@
-require './tic_tac_toe'
-require './game_ai'
-require './game_ui'
-require './game'
-
-Game.new.play
---
+++ play.rb
@@ -0,0 +1,6 @@
+require './game/tic_tac_toe'
+require './game/game_ai'
+require './game/game_ui'
+require './game/game'
+
+Game.new.play
