fc7a4fe4bbe5e5380fe06b6a27be999f490b72b2
xiranou <xiranou@gmail.com>
2015-02-06 21:22:59 -0500

    add tests for valid_move

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,6 +17,23 @@ describe "Game" do
     it "should set computer to 'o'" do
       expect(@game.computer).to eq('o')
     end
+    it "should let player take the 'o' mark" do
+      @game.set_mark(1)
+      expect(@game.player).to eq('o')
+    end
+  end
+
+  describe "valid_move?" do
+    before do
+      @game = Game.new
+      @game.board.mark(index: 0, mark: "x")
+    end
+    it "should return false when player pick a marked spot" do
+      expect(@game.valid_move?(0)).to be false
+    end
+    it "should return true when player pick an empty spot" do
+      expect(@game.valid_move?(1)).to be true
+    end
   end
 
 end
